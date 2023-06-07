8fd31d9add7ed96663627cb5bada9533959963ed
ckumler <corey.kumler@gmail.com>
2023-06-07 07:04:29 -0400

    project 4 finished

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -12,3 +12,69 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+// screen = screen start
+// screen_cur = current word of screen ram
+// screen_end = last word of screen ram
+// white = 0
+// black = -1
+//
+// if (keyboard != white) {
+//      draw(black)   
+// } else {
+//      draw(white)   
+// }
+// 
+// draw (color) {
+//  while (screen_cur <= screen_end){
+//      @screen_cur = color;
+//  }   
+// }
+
+
+
+(INIT)
+	@SCREEN
+	D=A
+	@screen_cur
+	M=D
+
+	@8192 // screen_width/16 * screen_height
+	D=D+A
+	@screen_end // screen_start + screen_total (in bytes)
+	M=D
+
+	@KBD
+	D=M
+	@CLEAR
+	D; JEQ // if keyboard input == 0, clear screen
+
+	@draw_color
+	M=-1 // else, set draw_color to black
+
+	@DRAW_SCREEN
+	0; JMP
+
+(CLEAR)
+	@draw_color
+	M=0 //set draw_color to white
+
+(DRAW_SCREEN)
+	@screen_cur
+	D=M
+	@screen_end
+	D=D-M
+	@INIT
+	D; JEQ // go back to init if finished drawing
+
+	@draw_color
+	D=M
+	@screen_cur
+	A=M
+	M=D // draw color to byte at cur
+
+	@screen_cur
+	M=M+1 //increment cur
+
+	@DRAW_SCREEN
+	0; JMP //loop
--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -10,3 +10,38 @@
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
 // Put your code here.
+
+// for (i=R1, R2=0; R1 > 0; i--) {
+//  R2 += R0
+// }
+// END
+
+
+@R1
+D=M
+@iterator
+M=D //set iterator to R1 (amount of times we'll add R0)
+
+@R2
+M=0 // initalize to 0
+
+(LOOP)
+@iterator
+D=M
+@END
+D; JEQ // if end of loop, end
+
+@iterator
+M=M-1 // decriment iterator
+
+@R0
+D=M //load R0 to data
+@R2
+M=D+M //add R0 to R2 (acc)
+
+@LOOP
+D; JMP
+
+(END)
+@END
+D; JMP
